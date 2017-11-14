6e43778bf076b76f81b65023e73c0ad37b2c389b
tanwm <1255116997@qq.com>
2017-11-14 22:01:37 +0800

    细节修改

--- js/index.js
+++ js/index.js
@@ -1,41 +1,44 @@
 var obj = document.getElementById("nav").getElementsByTagName("a");
-for(var i = 0;i < obj.length;i++){
-	(function(obj,i){
-		obj.onmouseover = function(){
-			obj.childNodes[1].style.top = "45%";
-			obj.childNodes[3].style.top = "45%";
-			// obj.parentNode.setAttribute("class","cur");
-			// console.log(obj.parentNode);
-		}
-		obj.onmouseout = function(){
-			obj.childNodes[3].style.top = "50%";
-			obj.childNodes[1].style.top = "50%";
-			// obj.parentNode.removeAttribute("class");
-		}
-	})(obj[i],i);
+for (var i = 0; i < obj.length; i++) {
+    (function(obj, i) {
+        obj.onmouseover = function() {
+            obj.childNodes[1].style.top = "45%";
+            obj.childNodes[3].style.top = "45%";
+            // obj.parentNode.setAttribute("class","cur");
+            // console.log(obj.parentNode);
+        }
+        obj.onmouseout = function() {
+            obj.childNodes[3].style.top = "50%";
+            obj.childNodes[1].style.top = "50%";
+            // obj.parentNode.removeAttribute("class");
+        }
+    })(obj[i], i);
 }
 // console.log(obj);
 var bannerDom = document.getElementById("banner");
 // console.log(bannerDom);
 var widths = [0 * -1920, 1 * -1920, 2 * -1920, 3 * -1920, 4 * -1920];
 var i = 1;
-if(bannerDom){
-	bannerDom.style.transition = 'all 1s';
+if (bannerDom) {
+    bannerDom.style.transition = 'all 1s';
 }
 
 setInterval(function() {
-	var dots = document.getElementById("dots").getElementsByTagName("li");
-	dots[i].setAttribute("class","cur");
-	if(i > 0){
-		dots[i-1].removeAttribute("class");
-	}
-	i++;
-	if(i == widths.length) {
-		i = 1;
-		dots[4].removeAttribute("class");
-		dots[i].setAttribute("class","cur");
-	}
-	bannerDom.style.marginLeft = widths[i] + 'px';
+    var dots = document.getElementById("dots");
+
+    // console.log(dots);
+    if (dots) {
+    	dots =dots.getElementsByTagName("li");
+        dots[i].setAttribute("class", "cur");
+        if (i > 0) {
+            dots[i - 1].removeAttribute("class");
+        }
+        i++;
+        if (i == widths.length) {
+            i = 1;
+            dots[4].removeAttribute("class");
+            dots[i].setAttribute("class", "cur");
+        }
+        bannerDom.style.marginLeft = widths[i] + 'px';
+    }
 }, 3000);
-
-
